a12d5a46cc9a157b94311b32b875f3120ca645df
RayhanBryan <rayhanbryan@gmail.com>
2022-05-15 02:16:00 +0700

    update

--- src/components/HomeVisiMisi.vue
+++ src/components/HomeVisiMisi.vue
@@ -1,15 +1,15 @@
 <template>
     <div class="grid justify-content-center">
-        <div class="col-12 lg:col-3 text-center lg:my-8" data-aos="zoom-in" data-aos-duration="2000">
-            <h1>VISI</h1>
-            <p>“Menebarkan Kebahagiaan dengan Menjadi Business Opportunity yang Menguntungkan bagi Semua Stakeholder”</p>
-            <h1>MISI</h1>
+        <div class="col-12 lg:col-3 text-center lg:my-8">
+            <h1 data-aos="zoom-in" data-aos-duration="2000">VISI</h1>
+            <p data-aos="zoom-in" data-aos-duration="2000">“Menebarkan Kebahagiaan dengan Menjadi Business Opportunity yang Menguntungkan bagi Semua Stakeholder”</p>
+            <h1 data-aos="zoom-in" data-aos-duration="2000">MISI</h1>
             <ol class="text-left">
-                <li>Mengutamakan pelayanan service excellent dan memudahkan Customer dalam menikmati/mendapatkan produk ESQUE.</li>
-                <li>Menciptakan produk yang bervariatif dan berkualitas dengan harga yang terjangkau.</li>
-                <li>Menebarkan kebahagiaan dengan memberikan dukungan/support kepada lingkungan masyarakat yang membutuhkan.</li>
-                <li>Menyediakan dan mensupport seluruh Karyawan untuk bertumbuh dari segi Knowledge & Spiritual.</li>
-                <li>Memastikan dana Investasi dikelola dengan Amanah dan Profesional.</li>
+                <li data-aos="zoom-in" data-aos-duration="2000">Mengutamakan pelayanan service excellent dan memudahkan Customer dalam menikmati/mendapatkan produk ESQUE.</li>
+                <li data-aos="zoom-in" data-aos-duration="2000">Menciptakan produk yang bervariatif dan berkualitas dengan harga yang terjangkau.</li>
+                <li data-aos="zoom-in" data-aos-duration="2000">Menebarkan kebahagiaan dengan memberikan dukungan/support kepada lingkungan masyarakat yang membutuhkan.</li>
+                <li data-aos="zoom-in" data-aos-duration="2000">Menyediakan dan mensupport seluruh Karyawan untuk bertumbuh dari segi Knowledge & Spiritual.</li>
+                <li data-aos="zoom-in" data-aos-duration="2000">Memastikan dana Investasi dikelola dengan Amanah dan Profesional.</li>
             </ol>
         </div>
     </div>
